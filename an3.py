356766c70d9d9f045fede9a7e70772d738063201
uknownash <47666850+uknownash@users.noreply.github.com>
2020-01-14 09:00:15 +0530

    Add files via upload

---
+++ an3.py
@@ -0,0 +1,19 @@
+import os
+c =	1
+d =	50
+i =	1
+while i <=	100:
+	if (i%2)==0:
+		c +=	1
+		d -=	1
+	a =	"#"*c
+	b =	"*"*d
+	os.system("clear")
+	print("\n")
+	if i <	100:
+		n =	2
+		print(f" {n}/5  : {a}{b}")
+	else:
+		n =	3
+		print(f" {n}/5  : {a}{b}")
+	i +=	1

